35dde4db43aa970215e799add63465cc7751a8d0
aifram <aifram@uwaterloo.ca>
2022-05-25 08:41:24 -0400

    Hello

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,43 +1,75 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom';
+import * as React from 'react';
 
-import Home from '../Home';
-import PrivateRoute from '../Navigation/PrivateRoute.js';
-
-
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      //
-    };
-  }
-
-  componentDidMount() {
-    //
-  }
+const textbooks = [
+  {
+    title: 'The Road to React',
+    url: 'https://www.roadtoreact.com/',
+    author: 'Wieruch, R.',
+    year: "2021",
+  }, {
+    title: 'Learning Node.js',
+    url: 'https://github.com/marcwan/LearningNodeJS',
+    author: 'Wandschneider, Marc',
+    year: "2017",
+  },
+];
 
 
-  componentWillUnmount() {
-    this.listener();
-  }
 
+const App = () => {
 
-  render() {
-    return (
-	  <Router>
-	    <div>
-        <PrivateRoute exact path="/" component={Home}/>
-	    </div>
-	  </Router>
-    );
-  }
+  return (
+    <div>
+      <h1>
+        React Lecture 3_2
+      </h1>
+
+      <Search />
+
+      <hr />
+
+      <List />
+
+    </div>
+  );
 }
+
+const Search = () => {
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input
+        id="search"
+        type="text" onChange={handleChange}
+      />
+
+    </div>
+  );
+};
+
+
+const List = () => {
+  return (
+    <ul>
+      {textbooks.map((item) => {
+        return (
+          <li>
+            <span>
+              <a href={item.url}>{item.url}</a>
+            </span>
+            <span> {item.title}</span>
+            <span>{" by " + item.author}</span>
+          </li>);
+      })}
+    </ul>
+
+  )
+}
+
 
 export default App;
